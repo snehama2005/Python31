f73cef68172c9745d56b1e30679b57cdacd264eb
snehama2005 <148319920+snehama2005@users.noreply.github.com>
2023-10-29 12:10:56 +0530

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,11 @@
+# #1 write a function to compute 5/0 and use try/except to catch the exception?
+# try:
+#     k = 5//0 
+#     print(k)     
+# except ZeroDivisionError:    
+#     print("Can't divide by zero") 
+        
+# finally: 
+
+#     print('This is always executed')  
+
